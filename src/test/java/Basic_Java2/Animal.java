1565d037f99c728fd5de127688b5d03b96837448
Rasedulhb <raseduls@gmail.com>
2023-02-13 21:31:06 -0500

    modified calculator

--- src/test/java/Basic_Java2/Animal.java
+++ src/test/java/Basic_Java2/Animal.java
@@ -3,6 +3,7 @@ package Basic_Java2;
 public class Animal {
 	public static void main(String[] args) {
 		System.out.println("This is my first coding");
+		
 	}
 
 }
--- src/test/java/Basic_Java2/Calculator.java
+++ src/test/java/Basic_Java2/Calculator.java
@@ -1,9 +1,21 @@
 package Basic_Java2;
-
 public class Calculator {
 
 	public static void main(String[] args) {
-		System.out.println("This is my calculator");
+		
+		int a = 90;
+		int b = 45;
+		int sum = a + b;
+		int sub = a - b;
+		int mul = a * b;
+		int divd = a / b ;
+		
+		
+		System.out.println("Addition between number A & B: " + sum);
+		System.out.println("Subtraction between number A & B: " + sub);
+		System.out.println("Multipication between number A & B: " + mul);
+		System.out.println("Division between number A & B: " + divd);
+		
 
 	}
 
